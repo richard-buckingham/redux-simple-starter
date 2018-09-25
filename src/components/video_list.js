69bca7b34ee04df3075708163d9153f9c45e763c
richie.buckingham <richie.buckingham@gmail.com>
2018-09-26 08:32:27 +1000

    clean up display of video_list_item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,7 @@ import VideoListItem from "./video_list_item";
 
 const VideoList = props => {
   const videoItems = props.videos.map(video => {
-    console.log("video = ", video);
+    //console.log("video = ", video);
     return <VideoListItem key={video.etag} video={video} />;
   });
 
