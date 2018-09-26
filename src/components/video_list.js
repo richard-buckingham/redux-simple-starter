1062c418af76b6f3247bb8e96eaba131b27b3aee
richie.buckingham <richie.buckingham@gmail.com>
2018-09-27 08:13:13 +1000

    minor refactor

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,6 @@ import VideoListItem from "./video_list_item";
 
 const VideoList = props => {
   const videoItems = props.videos.map(video => {
-    //console.log("video = ", video);
     return (
       <VideoListItem
         onVideoSelect={props.onVideoSelect}
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,8 +3,13 @@ import React from "react";
 const VideoListItem = ({ video, onVideoSelect }) => {
   const imageUrl = video.snippet.thumbnails.default.url;
 
+  const emitVideo = () => {
+    console.log("calling the passed in function to emit the Video");
+    return onVideoSelect(video);
+  };
+
   return (
-    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+    <li onClick={emitVideo} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
